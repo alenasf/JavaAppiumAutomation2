b30e880e688123f349ac8f8c0be0db7468efc445
Alena Pushniakova <alenapython@gmail.com>
2018-06-15 15:49:10 -0700

    Update path

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -24,7 +24,7 @@ public class FirstTest {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/alenapushniakova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("app", "/Users/alenapushniakova/Desktop/JavaAppiumAutomation2/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL( "http://127.0.0.1:4723/wd/hub"), capabilities);
     }
