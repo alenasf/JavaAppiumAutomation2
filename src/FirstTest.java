1d4f41f6a3702da564ef630ebb48373ac70745e4
Alena Pushniakova <alenapython@gmail.com>
2018-07-01 22:44:38 -0700

    3.05 Assert: basic

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -346,9 +346,42 @@ public class FirstTest {
                 "Cannot delete saved article",
                 5
         );
-
     }
 
+    @Test
+    public void testAmountOfNotEmptySearch()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        String search_line = "linkin Park discography";
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot put text into articles folder input",
+                5
+        );
+
+        String search_result_locator = "//*[resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        waitForElementPresent(
+                By.xpath(search_result_locator),
+                "Cannot find anything by the request " + search_line,
+                15
+        );
+
+
+        int amount_of_search_results = getAmountOfElements(
+                By.xpath(search_result_locator)
+        );
+        Assert.assertTrue(
+                "We found too few results",
+                amount_of_search_results > 0
+        );
+    }
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutinSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutinSeconds);
@@ -450,5 +483,11 @@ public class FirstTest {
                 .moveTo(left_x, middle_y)
                 .release()
                 .perform();
+    }
+    private int getAmountOfElements(By by)
+    {
+        List elements = driver.findElements(by);
+        return elements.size();
+
     }
 }
