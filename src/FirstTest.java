0dc96c5d7fe7af6e80bc7e25631b55b4632c5aae
Alena Pushniakova <alenapython@gmail.com>
2018-07-01 23:42:58 -0700

    3.08 Background: basics

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -472,6 +472,35 @@ public class FirstTest {
                 title_after_second_rotation
         );
     }
+    @Test
+    public void testSearchArticleInBackground()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id ='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+        driver.runAppInBackground(2);
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id ='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find article after returning from background",
+                5
+        );
+    }
 
 
 
