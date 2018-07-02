3b7be457a7a6a9394cba019e2edc5b36683ef968
Alena Pushniakova <alenapython@gmail.com>
2018-07-01 23:05:17 -0700

    3.06  Assert: assertion error

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -357,7 +357,7 @@ public class FirstTest {
                 5
         );
 
-        String search_line = "linkin Park discography";
+        String search_line = "linkin Park Diskography";
 
         waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text,'Search…')]"),
@@ -380,6 +380,38 @@ public class FirstTest {
         Assert.assertTrue(
                 "We found too few results",
                 amount_of_search_results > 0
+        );
+    }
+
+    @Test
+    public void testAmountOfEmptySearch()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+        String search_line = "jhjkjhkjhkhkh";
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot put text into articles folder input",
+                5
+        );
+
+        String search_result_locator = "//*[resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        String empty_result_label = "//*[@text='No results found']";
+
+        waitForElementPresent(
+                By.xpath(empty_result_label),
+                "Cannot find empty result label by the request" + search_line,
+                15
+        );
+
+        assertElementNotPresent(
+                By.xpath(search_result_locator),
+                "We've found some results by request " + search_line
         );
     }
 
@@ -488,6 +520,14 @@ public class FirstTest {
     {
         List elements = driver.findElements(by);
         return elements.size();
+    }
 
+    private void assertElementNotPresent(By by, String error_message)
+    {
+        int amount_of_elements = getAmountOfElements(by);
+        if (amount_of_elements > 0) {
+            String default_message = "An element " + by.toString() + "supposed to be not present";
+            throw new AssertionError( default_message + " " + error_message );
+        }
     }
 }
