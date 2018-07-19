ad98b8ca2f726d7b8abb871b47918974ac663a3a
Alena Pushniakova <alenapython@gmail.com>
2018-07-19 15:58:05 -0700

    4.01

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,58 +1,24 @@
 
-
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
-import java.net.URL;
 
 
-public class FirstTest {
 
-    private AppiumDriver driver;
+public class FirstTest extends CoreTestCase {
 
-    @Before
-    public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "7.1.1");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/alenapushniakova/Desktop/JavaAppiumAutomation2/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-
-//        Ex. 7 Screen rotation
-        driver.rotate(ScreenOrientation.PORTRAIT);
-
-    }
-
-
-    @After
-    public void tearDown()
-
-
-    {
-        driver.quit();
-    }
 
 
     @Test
-    public void firstTest() {
+    public void testSearch() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia' input",
@@ -261,7 +227,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList() {
+    public void testSaveFirstArticleToMyList() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia' input",
